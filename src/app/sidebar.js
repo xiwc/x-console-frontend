27f96b5fecdb11b07731bce61749fac6e3490e12
weicheng <weicheng.xi@newtouch.cn>
2016-07-20 17:40:51 +0800

    ...

--- src/app/sidebar.js
+++ src/app/sidebar.js
@@ -6,11 +6,15 @@ from 'aurelia-framework';
 
 @containerless
 export class Sidebar {
-	
+
     /**
      * 当视图被附加到DOM中时被调用
      */
     attached() {
-		
+        $('.nx-pp').popup({
+            onShow: () => {
+                return $('.nx-body').hasClass('nx-left-sidebar-menu-labeled-icon');
+            }
+        });
     }
 }
