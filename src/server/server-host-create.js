4c67aef5a9f40dfcc7b79b733d2523446898f549
weicheng xi <weicheng.xi@newtouch.cn>
2016-07-22 23:03:39 +0800

    添加创建主机steps

--- src/server/server-host-create.js
+++ src/server/server-host-create.js
@@ -3,17 +3,101 @@ import {
     containerless
 }
 from 'aurelia-framework';
+// import {
+//     sync
+// }
+// from 'aurelia-templating-resources';
 
 @containerless
 export class ServerHostCreate {
 
+    // @sync('.nx-steps-container .steps .step')
+    steps;
+
+    // @sync('.nx-steps-container .ui.tab')
+    tabs;
+
     /**
      * 当视图被附加到DOM中时被调用
      */
-    attached() {}
+    attached() {
+        this.steps = $(this.stepsContainer).find('.steps .step');
+        this.tabs = $(this.stepsContainer).find('.tab');
+        this.stepCnt = this.steps.size();
+    }
 
     show() {
-        $(this.modal).modal('show');
+        $(this.modal).modal({
+            onShow: () => {
+                this.reset();
+            }
+        }).modal('show');
+    }
+
+    reset() {
+        $.each(this.steps, function(index, step) {
+            if (index == 0) {
+                $(step).addClass('active').removeClass('disabled');
+            } else {
+                $(step).removeClass('active').addClass('disabled');
+            }
+        });
+        $.each(this.tabs, function(index, tab) {
+            if (index == 0) {
+                $(tab).addClass('active');
+            } else {
+                $(tab).removeClass('active');
+            }
+        });
+
+        $(this.btnPre).addClass('disabled');
+        $(this.btnOk).addClass('disabled');
+        $(this.btnNext).removeClass('disabled');
+    }
+
+    preHandler() {
+        let $step = $(this.steps).filter('.active.step');
+        let $tab = $(this.tabs).filter('.active.tab');
+        let step = parseInt($step.attr('data-step'));
+        if (step > 1) {
+            $step.removeClass('active');
+            $(this.steps).filter(`.step[data-step="${step - 1}"]`).removeClass('disabled').addClass('active');
+            $tab.removeClass('active');
+            $(this.tabs).filter(`.tab[data-step="${step - 1}"]`).addClass('active');
+            $(this.btnOk).addClass('disabled');
+            $(this.btnNext).removeClass('disabled');
+
+            if (step == 2) {
+                $(this.btnPre).addClass('disabled');
+            }
+        }
+    }
+
+    nextHandler() {
+        let $step = $(this.steps).filter('.active.step');
+        let $tab = $(this.tabs).filter('.active.tab');
+        let step = parseInt($step.attr('data-step'));
+        if (step < this.stepCnt) {
+            $step.removeClass('active');
+            $(this.steps).filter(`.step[data-step="${step + 1}"]`).removeClass('disabled').addClass('active');
+            $tab.removeClass('active');
+            $(this.tabs).filter(`.tab[data-step="${step + 1}"]`).addClass('active');
+            $(this.btnOk).addClass('disabled');
+            $(this.btnPre).removeClass('disabled');
+
+            if (step == this.stepCnt - 1) {
+                $(this.btnOk).removeClass('disabled');
+                $(this.btnNext).addClass('disabled');
+            }
+        }
+    }
+
+    cancelHandler() {
+        $(this.modal).modal('hide');
+    }
+
+    okHandler() {
+        $(this.modal).modal('hide');
     }
 
 }
