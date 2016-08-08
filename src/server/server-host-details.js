ed1b5def84854b8156fe270deb8b44d42af30e80
fan.xu <fan.xu@newtouch.cn>
2016-08-08 17:48:58 +0800

    html中表达式添加空格便于查看代码

--- src/server/server-host-details.js
+++ src/server/server-host-details.js
@@ -144,7 +144,7 @@ export class ServerHostDetails {
 
     outPrivateNetworkHandler(item) {
         this.confirm.show({
-            content: `确定要离开<code>${item.networkType=='1'?'经典网络':item.networkName}</code>吗?`,
+            content: `确定要离开<code>${item.networkType == '1' ? '经典网络' : item.networkName}</code>吗?`,
             onapprove: () => {
                 this.http.fetch(nsApi.url('host.privateNetwork.delete.post'), {
                     method: 'post',
