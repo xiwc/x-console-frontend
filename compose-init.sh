e3af46c04c5758714a00e4d7c202822d641e3af9
weicheng <weicheng.xi@newtouch.cn>
2016-08-11 15:07:06 +0800

    添加自动化脚本

--- compose-init.sh
+++ compose-init.sh
@@ -4,7 +4,9 @@ git pull
 npm install
 npm run build:prod
 
-cp ./locale ./dist/
+mkdir -p ./dist/login
+cp ./login/login.html ./dist/login/login.html
+cp -r ./locale ./dist/
 
 docker build -t 192.168.205.11:5000/x-console-frontend:test .
 
