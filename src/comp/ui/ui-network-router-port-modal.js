d1ff638ea01ff04bf3f25f4a6d7fff2d00d15dd4
minzhaocheng <zhaocheng.min@newtouch.cn>
2016-08-11 17:29:20 +0800

    新增端口转发页面UI。

---
+++ src/comp/ui/ui-network-router-port-modal.js
@@ -0,0 +1,56 @@
+import {
+    bindable,
+    containerless
+}
+from 'aurelia-framework';
+
+@containerless
+export class UiNetworkRouterPortModal {
+    @bindable name = "";
+    @bindable onapprove = ''; // 确认回调函数
+    @bindable ondeny = ''; // 取消回调函数
+
+    /**
+     * 当视图被附加到DOM中时被调用
+     */
+    attached() {
+        $(this.modal).find('.ui.dropdown').dropdown();
+        $(this.modal).find('.checkbox').checkbox();
+        $(this.modal).modal({
+            closable: false,
+            allowMultiple: true,
+            onShow: () => {
+                this.name = '';
+                $(this.form).form("reset");
+            },
+            onApprove: () => {
+                this.onapprove && this.onapprove();
+            },
+            onDeny: () => {
+                this.ondeny && this.ondeny();
+            }
+        });
+    }
+
+    /**
+     * 显示确认窗口
+     * @param onapprove: 确认回调函数
+     * @param ondeny: 取消回调函数
+     */
+    show(onapprove, ondeny) {
+        if (onapprove) {
+            this.onapprove = onapprove;
+        }
+
+        if (ondeny) {
+            this.ondeny = ondeny;
+        }
+
+        $(this.modal).modal('show');
+    }
+
+    /* 隐藏确认窗口 */
+    hide() {
+        $(this.modal).modal('hide');
+    }
+}
